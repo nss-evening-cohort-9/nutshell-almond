25ad83d78ff23cc3dbaf0d400a7aed12a9df988f
Matt Bell <matthewaustinbell@gmail.com>
2019-06-08 12:27:15 -0500

    auth

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,13 @@
+import firebase from 'firebase/app';
+
+import auth from './components/auth/auth';
+
+import MyNavbar from './components/auth/MyNavbar/MyNavbar';
+
+import authData from './helpers/data/authData';
+
+import apiKeys from './helpers/apiKeys.json';
+
 import '../styles/main.scss';
 import 'bootstrap';
 
@@ -12,7 +22,10 @@ document.getElementById('news').addEventListener('click', () => {
 });
 
 const init = () => {
-
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  MyNavbar.navbarEvents();
+  authData.checkLogInStatus();
+  auth.domStringBuilder();
 };
 
 init();
