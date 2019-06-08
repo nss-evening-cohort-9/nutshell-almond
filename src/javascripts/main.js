c13d6632ac6cb8a9e742810075dd41ad9952939e
Matt Bell <matthewaustinbell@gmail.com>
2019-06-08 12:47:33 -0500

    home hides and shows

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 
 import auth from './components/auth/auth';
 
-import MyNavbar from './components/auth/MyNavbar/MyNavbar';
+import MyNavbar from './components/MyNavbar/MyNavbar';
 
 import authData from './helpers/data/authData';
 
@@ -11,21 +11,21 @@ import apiKeys from './helpers/apiKeys.json';
 import '../styles/main.scss';
 import 'bootstrap';
 
-document.getElementById('diary').addEventListener('click', () => {
-  console.error('diary was clicked');
-});
-document.getElementById('events').addEventListener('click', () => {
-  console.error('events was clicked');
-});
-document.getElementById('news').addEventListener('click', () => {
-  console.error('news was clicked');
-});
-
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   MyNavbar.navbarEvents();
   authData.checkLogInStatus();
   auth.domStringBuilder();
+
+  document.getElementById('diary').addEventListener('click', () => {
+    console.error('diary was clicked');
+  });
+  document.getElementById('events').addEventListener('click', () => {
+    console.error('events was clicked');
+  });
+  document.getElementById('news').addEventListener('click', () => {
+    console.error('news was clicked');
+  });
 };
 
 init();
