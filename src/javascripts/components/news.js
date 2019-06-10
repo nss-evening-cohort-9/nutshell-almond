93a309453a66dd428cc778a5edda7cd45609724e
Lwam Tekie <ltekie87@yahoo.com>
2019-06-10 18:31:45 -0500

    wip

--- src/javascripts/components/news.js
+++ src/javascripts/components/news.js
@@ -1,6 +1,19 @@
+/* eslint-disable no-trailing-spaces */
+import util from '../helpers/util';
+import newsData from '../helpers/data/newsData';
+ 
+const newsStringBuilder = (uid) => {
+  newsData.getNewsByUid(uid).then((news) => {
+    let domString = `<h1>${news.title}</h1>`;
+    domString += `<input type="${news.articleUrl}"/>`;
+    util.printToDom('news', domString);
+  }).catch(err => console.error('could not get news', err));
+};
+
 const initNews = () => {
+  newsStringBuilder();
 };
 
 initNews();
 
-export default { initNews };
+export default { newsStringBuilder };
